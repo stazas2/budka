f1b42cf347fd86aac9902e7447cf0805a60ef72c
stazas2 <malyshev.stas03@mail.ru>
2025-02-15 19:52:58 +0200

    Refactor start.bat for improved execution flow, update config.json for visibility settings and camera mode, enhance logging in script.js for better error handling and user feedback, and include SavedPhotos directory in package.json.

--- canon/start.bat
+++ canon/start.bat
@@ -1,21 +1,19 @@
 @echo off
 REM Проверяем наличие файла pathExe.txt
 if exist "Server\pathExe.txt" (
-    start /B "" "Server/Api.exe"
+    start "" "Server/Api.exe"
     timeout /t 2 /nobreak > nul
     echo Файл pathExe.txt найден. Отправляю запрос на localhost:5000/api/post/reconnect
     curl -X POST http://localhost:5000/api/post/reconnect
     timeout /t 2 /nobreak > nul
-    start /B "" "Client/CameraControllerClient.exe"
+    start "" "Client/CameraControllerClient.exe"
     if %errorlevel% neq 0 (
         echo Ошибка при отправке запроса. Запускаю приложения как обычно.
         start /B "" "Server/Api.exe"
-        @REM start /B "" "Server/Api.exe"
         timeout /t 2 /nobreak > nul
         start /B "" "Camera/CameraControl.exe"
         timeout /t 2 /nobreak > nul
         start /B "" "Client/CameraControllerClient.exe"
-        @REM start /B "" "Client/CameraControllerClient.exe"
     )
 ) else (
     echo Файл pathExe.txt не найден. Запускаю приложения как обычно.
